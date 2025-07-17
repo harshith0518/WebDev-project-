27f3cc58cb49b15fa8c5fd3efc548010602d12fd
harshith0518 <168824949+harshith0518@users.noreply.github.com>
2025-07-17 09:19:28 +0530

    Delete solution directory

--- solution/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from solution.views import displaySolution,testingPurpose
-
-
-urlpatterns = [
-    path('solution/<int:solution_id>',displaySolution,name = 'solution-details'),
-    path('submit/<int:problem_id>/',testingPurpose,name = 'code-solution'),
-    
-]
